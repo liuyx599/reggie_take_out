db2d83ae6618fbba7fb8869553357c125f8092d1
onebear <995066544@qq.com>
2023-04-05 20:02:51 +0800

    #day04  图片上传功能

---
+++ src/test/java/com/itheima/test/UploadFileTest.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+import org.junit.jupiter.api.Test;
+
+public class UploadFileTest {
+    @Test
+    public void test1(){
+        String fileName = "asd.jpg";
+        System.out.println(fileName);
+    }
+}
